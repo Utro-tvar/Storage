147a07cad99717aca9d1a8e77091aa380085a6f1
Utro <utrotime@gmail.com>
2024-10-10 00:28:19 +0300

    Add logger initialization

--- cmd/storage/main.go
+++ cmd/storage/main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"flag"
+	"log"
+	"log/slog"
+	"os"
 
 	"github.com/Utro-tvar/Storage/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
@@ -13,5 +22,33 @@ func main() {
 
 	cfg := config.MustLoad(*configPath)
 
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting storage", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+
 	_ = cfg
+}
+
+func setupLogger(env string) *slog.Logger {
+	var logger *slog.Logger
+
+	switch env {
+	case envLocal:
+		logger = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	default:
+		log.Fatalf("cannot find logger for environment: %s", env)
+	}
+
+	return logger
 }
