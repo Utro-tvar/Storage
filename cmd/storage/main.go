b3fce592a729ab998f48796098dde8cd306feb86
Utro <utrotime@gmail.com>
2024-10-10 00:05:54 +0300

    Add config load.

--- cmd/storage/main.go
+++ cmd/storage/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"flag"
 
+	"github.com/Utro-tvar/Storage/internal/config"
+)
+
+func main() {
+	configPath := flag.String("config", "", "Path to config file")
+
+	flag.Parse()
+
+	cfg := config.MustLoad(*configPath)
+
+	_ = cfg
 }
