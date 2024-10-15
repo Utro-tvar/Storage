426caa1dbaf4a8621ff7244a73b8aebc72ed68d2
Utro <utrotime@gmail.com>
2024-10-15 14:56:20 +0300

    Modify database structure

--- internal/db/sqlite/sqlite.go
+++ internal/db/sqlite/sqlite.go
@@ -23,6 +23,7 @@ func New(storagePath string) (*Storage, error) {
 	CREATE TABLE IF NOT EXISTS resources(
 		id INTEGER PRIMARY KEY,
 		name TEXT NOT NULL,
+		type TEXT NOT NULL,
 		path TEXT NOT NULL);
 	`)
 
